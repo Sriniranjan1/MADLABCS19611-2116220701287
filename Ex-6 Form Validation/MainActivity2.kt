28fc025babf49970bf50186026d47bddea9c7aa7
Sriniranjan1 <220701287@rajalakshmi.edu.in>
2025-05-11 18:33:54 +0530

    Add files via upload

---
+++ Ex-6 Form Validation/MainActivity2.kt
@@ -0,0 +1,10 @@
+package com.example.formvalidation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
